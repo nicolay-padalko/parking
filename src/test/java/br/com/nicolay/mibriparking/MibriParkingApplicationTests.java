2ac49ff9cce9cd35aa8c9e56af505526acd3cf42
Nicolay Padalko <nicolay.padalko@gmail.com>
2022-02-10 10:20:49 -0300

    Initial commit

---
+++ src/test/java/br/com/nicolay/mibriparking/MibriParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nicolay.mibriparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MibriParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
